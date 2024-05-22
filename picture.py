88125575c18b2db30a8fcf7ce77975e2a9372e20
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-22 11:34:28 -0500

    Método join() implementado: Usamos el operador de destructuración para concatenar los caractes de cada elemento en la misma posición de las dos listas.

--- picture.py
+++ picture.py
@@ -25,7 +25,6 @@ class Picture:
     horizontal = []
     for value in range(size):
       horizontal.append(self.img[size - 1 - value])
-
     return Picture(horizontal)
 
   # Usamos el método _invColor() para cambiar de caracter y en consecuencia 
@@ -40,7 +39,10 @@ class Picture:
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    return Picture(None)
+    join = []
+    for i in range(len(self.img)):
+      join.append([*self.img[i], *p.img[i]])
+    return Picture(join)
 
   def up(self, p):
     return Picture(None)
