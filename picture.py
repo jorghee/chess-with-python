0e8f9384ed89f5d31915c6fe88b3d1cd155de207
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-22 11:25:17 -0500

    Usamos la 'compresión de listas en python' para crear la nueva cadena de caracteres invertidos para implmentar el método negative()

--- picture.py
+++ picture.py
@@ -18,6 +18,7 @@ class Picture:
     	vertical.append(value[::-1])
     return vertical
 
+  # Iterando desde el último elemento de la listas hacia el primer elmemento
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
     size = len(self.img)
@@ -27,9 +28,14 @@ class Picture:
 
     return Picture(horizontal)
 
+  # Usamos el método _invColor() para cambiar de caracter y en consecuencia 
+  # cambiar la representación de dicho caracter
   def negative(self):
     """ Devuelve un negativo de la imagen """
-    return Picture(None)
+    inverter = []
+    for value in self.img:
+      inverter.append([self._invColor(caracter) for caracter in value])
+    return Picture(inverter)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
