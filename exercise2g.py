affceeaaa32412fdfa2f1dab1ac9e43820f0387d
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-24 16:22:10 -0500

    Ejercicio 7 terminado: Usando todos los métodos
    Hemos creado 3 patrones base, la fila de cuadrados intercalados 'squares',
    la fila de las diferentes piezas 'pieces'y la fila de los peones 'pawns'.
    Primero nos enfocamos en crear las piezas negras, para ello hemos
    utilizado el método join(), pues las piezas estan una a lado de otra,
    luego esta fila de piezas hemos cambiado de color y la hemos superpuesto
    encima de 'squares'. Lo mismo hacemos con peones usando horizontalRepeat()
    y cambiando de color. En este caso, tanto a los 'pawns' como a 'squares'
    tienes que cambiar de color usando negative(), luego superponemos con
    under().
    Finalmente para las piezas blancas solo cambiamos de color usando
    negative() y luego juntamos estas figuras usando el método up().

---
+++ exercise2g.py
@@ -0,0 +1,22 @@
+from interpreter import draw
+from chessPictures import *
+
+squares = square.join(square.negative()).horizontalRepeat(4)
+piecesPart = rock.join(knight).join(bishop)
+
+# Part black
+pieces = squares.under(piecesPart.join(queen).join(king).join(piecesPart).negative())
+pawns = squares.under(pawn.horizontalRepeat(8)).negative()
+partBlack = pieces.up(pawns)
+
+# Part empty
+tableEmpty = squares.up(squares.negative()).verticalRepeat(2)
+
+# Part white
+partWhite = pawns.up(pieces).negative()
+
+# table
+table = partBlack.up(tableEmpty).up(partWhite)
+
+draw(table)
+
