50e23c2bcb85f61caa8b7ab5bc90ae38ff4cabcd
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-24 14:44:11 -0500

    EJERCICIO 1 TERMINADO, SE USÓ LOS MÉTODOS join() Y negative()
    Creamos una variable table que referencia a dos caballos creados en fila
    con el método join(). Antes de pasar el argumento a join() invertimos
    los caracteres del objeto y asi creamos la primera fila.
    Despues mofificamos la misma variable 'table' usando up() y antes de
    pasar como argumento el mismo 'table' le invertimos los caracteres
    usando negative().

--- testChess.py
+++
@@ -1,8 +0,0 @@
-from interpreter import draw
-from chessPictures import *
-
-# Ocurre un error en tiempo de ejecución
-# draw(king.join(queen).under(square))
-
-# Solución no esperada, king.join.(queen) se recorta
-draw(square.under(king.join(queen)))
