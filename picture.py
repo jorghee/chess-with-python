69a16a825da4ec9361488c37e03cb5bf138cf8a8
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-24 15:09:05 -0500

    Retornando un objeto Picture, no una lista de cadenas de texto.

--- picture.py
+++ picture.py
@@ -16,7 +16,7 @@ class Picture:
     vertical = []
     for value in self.img:
     	vertical.append(value[::-1])
-    return vertical
+    return Picture(vertical)
 
   # Iterando desde el último elemento de la listas hacia el primer elmemento
   def horizontalMirror(self):
